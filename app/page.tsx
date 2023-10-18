c0aa184499ddf9b9e47b6b7ef711333ce7726a34
seanbruce <seanbruce520@gmail.com>
2023-10-18 11:16:08 +0800

    :feat commit before add ui framework

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,227 @@
-import Image from 'next/image'
+const uuid = () => Math.random().toString(36).slice(2);
+
+const navMenus = [
+  {
+    id: uuid(),
+    title: "關于工會",
+    children: [
+      {
+        id: uuid(),
+        title: "本會簡介",
+      },
+      {
+        id: uuid(),
+        title: "理事長的話",
+      },
+      {
+        id: uuid(),
+        title: "歷屆理事長",
+      },
+      {
+        id: uuid(),
+        title: "理監事組織表",
+      },
+      {
+        id: uuid(),
+        title: "各委員會施政理念",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    title: "消息公告",
+    children: [
+      {
+        id: uuid(),
+        title: "會務公告",
+      },
+      {
+        id: uuid(),
+        title: "醫事公告",
+      },
+      {
+        id: uuid(),
+        title: "友會/校友會公告",
+      },
+      {
+        id: uuid(),
+        title: "大會專區",
+      },
+      {
+        id: uuid(),
+        title: "社福團體活動公告",
+      },
+      {
+        id: uuid(),
+        title: "七院校牙醫學系學生下鄉課輔活動",
+      },
+      {
+        id: uuid(),
+        title: "新型冠狀病毒防疫專區",
+      },
+      {
+        id: uuid(),
+        title: "牙醫師執業執照到期專區",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    title: "活動看板",
+    children: [
+      {
+        id: uuid(),
+        title: "行事曆",
+      },
+      {
+        id: uuid(),
+        title: "活動快訊",
+      },
+      {
+        id: uuid(),
+        title: "線上報名",
+      },
+      {
+        id: uuid(),
+        title: "活動花絮",
+      },
+      {
+        id: uuid(),
+        title: "會員專區",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    title: "會員專區",
+    children: [
+      {
+        id: uuid(),
+        title: "基本資料",
+      },
+      {
+        id: uuid(),
+        title: "會員福利",
+      },
+      {
+        id: uuid(),
+        title: "繼續教育積分查詢",
+      },
+      {
+        id: uuid(),
+        title: "應繳費用查詢",
+      },
+      {
+        id: uuid(),
+        title: "繳費紀錄查詢",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    title: "口衛專案計畫",
+  },
+  {
+    id: uuid(),
+    title: "健保專區",
+    children: [
+      {
+        id: uuid(),
+        title: "輔導追蹤作業流程",
+      },
+      {
+        id: uuid(),
+        title: "健保相關資料查詢",
+      },
+      {
+        id: uuid(),
+        title: "保險公告",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    title: "醫療法規",
+    children: [
+      {
+        id: uuid(),
+        title: "法規公告",
+      },
+      {
+        id: uuid(),
+        title: "待審法規",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    title: "牙醫助理專區",
+    children: [
+      {
+        id: uuid(),
+        title: "相關辦法",
+      },
+      {
+        id: uuid(),
+        title: "活動訊息",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    title: "下載專區",
+    children: [
+      {
+        id: uuid(),
+        title: "公會會籍申辦資料",
+      },
+      {
+        id: uuid(),
+        title: "衛生局執業申辦資料",
+      },
+      {
+        id: uuid(),
+        title: "申請健保特約流程",
+      },
+      {
+        id: uuid(),
+        title: "會費信用卡授權書",
+      },
+      {
+        id: uuid(),
+        title: "其他資料下載",
+      },
+      {
+        id: uuid(),
+        title: "手術同意書表下載",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    title: "線上服務",
+    children: [
+      {
+        id: uuid(),
+        title: "線上問卷",
+      },
+      {
+        id: uuid(),
+        title: "線上學習",
+      },
+    ],
+  },
+  {
+    id: uuid(),
+    title: "北市牙醫雜誌",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <>
+      <header className="h-[100px] bg-gray-100"></header>
+      <nav className="min-h-[40px] bg-emerald-500 container mx-auto"></nav>
+    </>
+  );
 }
